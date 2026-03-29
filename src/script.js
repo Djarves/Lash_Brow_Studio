0f9bc4c5658124476faf26fe533a91b646c6ec1d
Djarves <aneresimpelev@mail.ru>
2026-03-29 11:50:27 +0300

    feat: добавлены секции услуг и галерея с попап, адаптив и интерактив

--- src/script.js
+++ src/script.js
@@ -2,6 +2,14 @@ const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
 const overlay = document.querySelector('.overlay');
 const body = document.body;
+const images = document.querySelectorAll('.gallery__img');
+const popup = document.getElementById('popup');
+const popupImg = document.querySelector('.popup__img');
+const popupClose = document.querySelector('.popup__close');
+const arrowLeft = document.querySelector('.popup__arrow--left');
+const arrowRight = document.querySelector('.popup__arrow--right');
+
+let currentIndex = 0;
 
 // открытие/закрытие меню по бургеру
 burger.addEventListener('click', () => {
@@ -38,3 +46,70 @@ document.querySelectorAll('.header__nav').forEach(link => {
     window.scrollTo({ top: y, behavior: 'smooth' });
   });
 });
+
+// открыть
+images.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    currentIndex = index;
+    showImage();
+
+    popup.classList.add('active');
+    document.body.classList.add('lock'); // 🔥 блок скролла
+  });
+});
+
+// показать картинку
+function showImage() {
+  popupImg.src = images[currentIndex].src;
+}
+
+// закрыть
+function closePopup() {
+  popup.classList.remove('active');
+  document.body.classList.remove('lock'); // 🔥 вернуть скролл
+}
+
+popupClose.addEventListener('click', closePopup);
+
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    closePopup();
+  }
+});
+
+// стрелки
+arrowRight.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage();
+});
+
+arrowLeft.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage();
+});
+
+
+let startX = 0;
+
+popup.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+popup.addEventListener('touchend', (e) => {
+  let endX = e.changedTouches[0].clientX;
+
+  if (startX - endX > 50) {
+    // свайп влево
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage();
+  }
+
+  if (endX - startX > 50) {
+    // свайп вправо
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage();
+  }
+});
+
+
+
